5718c6ea39132e38914dcca41911ebdc1cfa41ab
MICHAEL WEINER <mweiner325@gmail.com>
2016-08-18 18:10:02 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  counter = 0
+  board.each do |element|
+    if element == "X" || element == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+
+def current_player(board)
+  counter = turn_count(board)
+  if counter % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
