713bb8a6b72c12b5f799c81b35a81b3ca0b84b2e
Siyu Bao <bao22s@mtholyoke.edu>
2016-08-31 07:39:29 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+  turns = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      turns += 1
+    end
+  end
+  turns
+end
+
+def current_player(board)
+  if turn_count(board).odd?
+    "O"
+  else
+    "X"
+  end
+end
