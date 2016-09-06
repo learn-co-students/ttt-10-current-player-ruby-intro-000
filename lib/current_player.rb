5275f0491f71aa1295be1dd3f8a3d78600ed1428
derek maloney <derekjmaloney@gmail.com>
2016-09-06 19:49:10 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,18 +1,22 @@
+
+
 def turn_count(board)
   counter = 0
   board.each do |count|
     if count == "X" || count == "O"
       counter += 1
-      return counter
+    end
   end
+  return counter
 end
 
 
 def current_player(board)
+counter = turn_count(board)
   turn_count(board)
     if counter % 2 == 0
-      return "O"
-    else
       return "X"
+    else
+      return "O"
     end
 end
