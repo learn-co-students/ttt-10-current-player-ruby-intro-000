55529eccd9a8880608ea9501a9f98fb02cb4697e
Feifei Wu <ffwu2001@gmail.com>
2016-09-15 00:32:46 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+def turn_count( board )
+  count = 0
+  board.each do |z|
+    if( z != " ")
+      count += 1
+      puts "found #{count} #{z}"
+    end
+  end
+  return count
+end
+
+
+def current_player( board )
+  if( turn_count( board ) % 2 == 0 )
+      return "X"
+  else
+    return "O"
+  end
+
+end
+
+# puts current_player( ["O", " ", " ", " ", "X", " ", " ", " ", "X"] )
