e1a56813758593cc776f80bf3effc0bb9d8856df
Victoria Machen <victoriamachen@gmail.com>
2016-09-16 13:15:59 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+
+def turn_count(board)
+  counter = 0
+
+    board.each do |turns|
+      if turns == "X" || turns == "O"
+        counter += 1
+      end
+    end
+  return counter
+end
+
+def current_player(board)
+  turns = turn_count(board)
+
+  if turns % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
