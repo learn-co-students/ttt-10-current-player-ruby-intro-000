ad6d9799d492b1bc65ec06ff4deaa35f921b8e2c
Christiane Heiligers <tina.heiligers@gmail.com>
2016-09-28 22:55:59 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,11 +9,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) %2 == 0
-    return "X"
-  else
-    return "O"
-  end
+  turn_count(board).even? ? "X" : "O"
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
