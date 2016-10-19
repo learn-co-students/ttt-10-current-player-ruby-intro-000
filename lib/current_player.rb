5fa571af7093c61bfb467a93bdde33cd9680515c
frank warren <frank.warren@gmail.com>
2016-10-19 23:15:13 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+
+def turn_count(board)
+  counter= 0
+    board.each do |turn|
+  if  turn == "X" || turn == "O"
+       counter += 1
+     end
+end
+return counter
+end
+
+def current_player(board)
+
+  if board.empty?
+    return "X"
+  end
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+    end
+ end
