42fce9b0eca76ff8fe2801b92fd5364348dafade
Lori Sanders <lori.sanders@me.com>
2016-10-31 15:18:27 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+  turns = 0
+  board.each do |char|
+    if char == "X" || char == "O"
+      turns += 1
+    end
+  end
+  return turns
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    current_player = "X"
+  else
+    current_player = "O"
+  end
+end
