ffe9d342b2f18ec181dd090759bdc0cb789d5609
Lori Sanders <lori.sanders@me.com>
2016-10-31 15:20:23 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,5 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    current_player = "X"
-  else
-    current_player = "O"
-  end
+  turn_count(board) % 2 == 0 ? current_player = "X" : current_player = "O"
 end
