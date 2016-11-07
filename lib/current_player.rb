7e5db3a62d746a1242252715da64ca8cebcccc2d
Michal Vacek <vahyno@gmail.com>
2016-11-07 19:05:42 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,22 @@
-board = Array.new(9, " ")
-
-counter = 0
 def turn_count(board)
-  if
-   counter += 1
- end
+ board.count{|token| token == "X" || token == "O"}
+
+  #turns = 0
+  #board.each do |token|
+  #  if token == "X" || token == "O"
+  #    turns += 1
+  #  end
+  #end
+  #turns
 end
+
+
+#counter = 0
+#def turn_count(board)
+#  if
+#   counter += 1
+# end
+#end
 
 def current_player(board)
   if turn_count(board) % 2 == 0
