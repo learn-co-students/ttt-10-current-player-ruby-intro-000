97576c445c59dac3297a022b0690d23a517875c3
Jonathan Forrider <jd4rider@gmail.com>
2016-12-05 08:02:32 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,9 @@
+def turn_count(board)
+  counter = 0
+  board.each{|place| place == 'O' || place == 'X'? counter += 1 : counter += 0 }
+  return counter
+end
+
+def current_player(board)
+  return turn_count(board) % 2 == 0? 'X' : 'O'
+end
