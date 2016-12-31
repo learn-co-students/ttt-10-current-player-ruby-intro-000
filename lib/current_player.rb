9e4709a6f1764fca080dc61d7ef7c5da7cb7ee05
Rob Ellis <rob.ellis357@gmail.com>
2016-12-31 13:29:09 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,23 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def turn_count(board)
   count = 0
-  board.each do |item|
-    count +=1
+  board.each do |i|
+    if i == "X" || i == "O"
+      count += 1
+    end
   end
   return count
 end
+
+turn_count(board)
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+current_player(board)
