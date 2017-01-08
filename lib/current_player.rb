8c0ff1b584053e76494d6087f7114df82fe5d8da
Rebecca Fisher <reb3cca.fisher@gmail.com>
2017-01-08 05:22:37 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,5 @@
 def current_player(board)
-    if turn_count(board) % 2 == 0
-      "X"
-    elsif turn_count(board) % 2 == 1
-      "O"
-    end
+    turn_count(board) % 2 == 0 ? "X" : "O"
 end
 
 def turn_count(board)
