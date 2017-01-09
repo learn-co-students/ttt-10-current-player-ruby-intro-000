a2db673d69289a19a941cf4255e1cd6787552bc3
Dana Cho <im.dana@gmail.com>
2017-01-09 22:56:31 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,22 +8,11 @@ def turn_count(board)
   return number_of_turns.count
 end
 
-
 def current_player(board)
-  x_array = []
-  o_array = []
-  board.each do |turn|
-    if turn == "X"
-      x_array.push("X")
-    end
-    if turn == "O"
-      o_array.push("O")
-    end
-  end
-  if x_array.length > o_array.length
-    return "O"
-  else
-    x_array.length == o_array.length
+  if turn_count(board).even?
     return "X"
-    end
+  else
+    turn_count(board).odd?
+    return "O"
+  end
 end
