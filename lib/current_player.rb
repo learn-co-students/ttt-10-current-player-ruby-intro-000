bd2e12783ae2209026703443565c574c1a815eff
Tiago Lopes <cloudbreakinggames@gmail.com>
2017-02-08 17:24:24 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,1 +1,18 @@
+def turn_count(board)
+turn = 0
+  board.each do |occupied_position|
+    if occupied_position == "X" || occupied_position == "O"
+      turn += 1
+    end
+  end
+  turn
+end
 
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
