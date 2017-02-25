5e9a7d294e8fde7dfcba2753a2c2ddc62ee30891
Jelo Yambao <jelolearns@gmail.com>
2017-02-25 19:35:09 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+  count = 0
+  board.each do |square|
+    if square.include?("X") || square.include?("O")
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
