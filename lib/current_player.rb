8a86a56ba76ccd40ccb05862f4efe880074c698f
aaron wilswe <element1028@gmail.com>
2017-04-01 18:28:01 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,11 +9,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) == 0
-    player = "X"
-  elsif turn_count(board) == 1
-    player = "O"
-  elsif turn_count(board) % 2 == 0
+  if turn_count(board) % 2 == 0
     player = "X"
   else
     player = "O"
