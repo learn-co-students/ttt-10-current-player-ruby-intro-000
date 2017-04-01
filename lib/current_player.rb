b1b8b70a0b4b810fc5d0c3cf2215984d2442183d
aaron wilswe <element1028@gmail.com>
2017-04-01 18:22:48 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,23 @@
+def turn_count(board)
+  turn = 0
+  board.each do |xo|
+    if xo == "X" || xo == "O"
+      turn += 1
+    end
+  end
+    turn
+end
+
+def current_player(board)
+  player = ""
+  if turn_count(board) == 0
+    player = "X"
+  elsif turn_count(board) == 1
+    player = "O"
+  elsif turn_count(board) % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
+  player
+end
