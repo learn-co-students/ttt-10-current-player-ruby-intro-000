09c2cf9917f22c699549ca0ea513584b55f6eafc
Sara Traband <straband@gmail.com>
2017-04-10 16:33:42 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+
+
+
+def turn_count(board)
+    count = 0
+    board.each do |number|
+      if number == "X" || number == "O"
+        count += 1
+      end
+    end
+  count
+end
+
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
