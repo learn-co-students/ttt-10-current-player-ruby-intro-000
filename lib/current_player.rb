efa0ded51a165f1ece84548b5454180cece01373
Robert Downey <countrysc@yahoo.com>
2017-04-23 00:47:20 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  counter = 0
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter +=1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  counter = turn_count(board)
+  if counter % 2
+    "X"
+  else
+    "O"
+  end
+end
