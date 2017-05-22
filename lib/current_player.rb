f6c7e6c160d03ede71278d9b25dc88011f53e2b7
Yisroel Reiss <mazel123@gmail.com>
2017-05-22 09:27:41 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+   counter = 0
+   board.each do |position|
+     if position != " " && position != "" && position != nil
+       counter += 1
+     end
+   end
+   return counter
+ end
+ 
+ def current_player(board)
+   if turn_count(board) % 2 == 0
+     "X"
+   else
+     "O"
+   end
+ end
