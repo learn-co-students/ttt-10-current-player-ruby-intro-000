52a1b26d5880a70cd2d71e8dea9f1eb702fa913e
Donald Leistman <donleistman@gmail.com>
2017-05-31 00:04:04 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+    counter = 0
+    board.each do |space|
+        if space == "X" || space == "O"
+            counter +=1
+        end
+    end
+    return counter
+end
+
+
+def current_player(board)
+    turn = turn_count(board)
+    if turn % 2 == 0
+        "X"
+    else
+        "O"
+    end
+end
