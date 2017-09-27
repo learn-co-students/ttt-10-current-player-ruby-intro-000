c45110ccf43ceadf92e8bd7fe3462d3ef696d1e5
Greg Potter <gpnyc2003@yahoo.com>
2017-09-27 15:13:50 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,26 @@
+def turn_count(board)
+  count = 0
+   board.each do |position|
+     if position == "X" || position == "O"
+       count+=1
+     end
+   end
+  count
+ end
+
+ def current_player(board)
+   countX = 0
+   countO = 0
+   board.each do |position|
+     if position == "X"
+       countX+=1
+     elsif position == "O"
+       countO+=1
+     end
+   end
+   if countX > countO
+     "O"
+   else
+     "X"
+   end
+ end
