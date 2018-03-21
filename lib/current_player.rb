0bbed584ac64d3f28561a4cb8632bf2a802f47a9
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-03-21 15:01:57 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+def turn_count(board)
+  count = 0
+  board.each do |place|
+    if place == "X" || place == "O"
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
