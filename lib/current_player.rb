cadbc7024bbabda39ac7af4dd8d49a73697e50a6
Al3nMicL <https://github.com/Al3nMicL>
2018-03-24 15:48:04 -0400

    Refactor

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,21 +3,13 @@ board = [" "," "," "," "," "," "," "," "," "]
 def turn_count(board)
   counter = 0
   board.each do |space|
-    if space == 'X'
+    if space == 'X' || space == 'O'
       counter += 1
-    elsif space == 'O'
-      counter += 1
-    else
-      nil
     end        
   end
   counter
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    "X"
-  else
-    "O"
-  end
+  turn_count(board).even? ? "X" : "O"
 end
