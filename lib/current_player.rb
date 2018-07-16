2d2091f096f59a3a6fed52fdc31cc18974d78eb2
kzavgorodnya <kateryna.zavgorodnya@gmail.com>
2018-07-16 19:47:37 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,17 +5,11 @@ def turn_count (board)
     end
   return counter
 end
-# 
-# def current_player (board)
-#   if counter % 2 == 0
-#     return "X"
-#   else
-#     return "O"
-# end
-# end
 
-#  Try to implement an iterator, like #each, to loop over the elements of the board array.
-#Remember that the return value of #each is the original array on which you are calling that method.
-#Keep in mind the desired return value of the #turn_count method is the number of turns that have been played.
-# We will need to keep track of the number of turns by setting some sort of counter and starting it at 0.
-# We will need to iterate over each member of the board array, check to see if that element is an "X" or an "O". If it is, we increment our counter variable by 1.
+def current_player (board)
+  if turn_count(board)% 2 == 0
+    return "X"
+  else
+    return "O"
+end
+end
