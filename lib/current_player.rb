914ebf61adf2ffb5a8d1ae5a3858ea401064b8f4
Lois Lee <lois.lee@me.com>
2018-10-12 23:33:56 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,20 +1,18 @@
-def turn_count (board)
-counter = 0
-  board.each do |turn|
-    if turn == "X" || turn == "O"
-      counter +=1
-    else
-      counter == counter
+def turn_count(board)
+  counter = 0
+  board.each do |board|
+    if "#{board}" != " "
+      counter += 1
     end
   end
-    return counter
-  end
+  counter
+end
 
 
 def current_player(board)
-  if turn_count(board)%2 == 0
-    return "X"
+  if turn_count(board) % 2 == 0
+    "X"
   else
-    return "O"
+    "O"
   end
 end
