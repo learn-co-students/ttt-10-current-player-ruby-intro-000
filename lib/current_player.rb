7221bc67329dfd6c61a76d6e9143112257784abb
Herna Michel <labellehernamichel@yahoo.fr>
2019-02-12 17:51:44 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+#code the methods turn_count and current_player here
+def turn_count(board)
+  counter = 0
+  board.each do |position|
+    if position == "X" || position == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
