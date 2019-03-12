466856820de87aa6dd7e32fb699b348efdbe9c10
JENNIFER OH <ohjennifer.90@gmail.com>
2019-03-12 10:12:31 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,19 +1,19 @@
 def turn_count(board)
   counter = 0
-
-  board.each do |position|
-    if position == "X" || position == "O"
-    counter += 1
+  board.each do |character|
+    if character == "X" || character == "O"
+      counter += 1
+    end
   end
+  counter
 end
-counter
 
-end
+
+
 
 
 def current_player(board)
-  turn = turn_count(board)
-  if turn % 2 == 0
+  if turn_count(board) % 2 == 0
     "X"
   else
     "O"
