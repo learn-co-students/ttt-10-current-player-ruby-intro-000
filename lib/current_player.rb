19e91532916bf0193a2a93c5891f742fb2180761
granit <graniitkastrati@gmail.com>
2019-10-22 16:23:27 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " " ]
+
+def turn_count(board)
+  counter = 0
+    board.each do |space|
+
+      if space == "X" || space == "O"
+        counter += 1
+      end
+    end
+  return counter
+end
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
