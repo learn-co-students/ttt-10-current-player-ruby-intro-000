4323a9a39bd40dec50351edb9516b81991c5961b
jose tavarez <newyorkcityjose@gmail.com>
2019-11-15 01:39:08 +0000

    Done.

--- spec/current_player_spec.rb
+++ spec/current_player_spec.rb
@@ -1,13 +1,12 @@
 require_relative '../lib/current_player.rb'
 
+
 describe "./lib/current_player.rb" do
   describe '#turn_count' do
     it 'counts occupied positions' do
-      board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
-      board2 = ["O", " ", " ", " ", "X", " ", " ", "O", "X"]
+      board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
 
-      expect(turn_count(board1)).to eq(3)
-      expect(turn_count(board2)).to eq(4)
+      expect(turn_count(board)).to eq(3)
     end
   end
 
@@ -29,5 +28,5 @@ describe "./lib/current_player.rb" do
 
       expect(current_player(board)).to eq("X")
     end
-  end  
+  end
 end
