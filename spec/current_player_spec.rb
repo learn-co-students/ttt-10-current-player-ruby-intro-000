081dd9faf96010278d00a0528101efdc10545e50
dazzling-acrobat-7297 <abhips@gmail.com>
2020-02-11 12:12:29 +0000

    Done.

--- spec/current_player_spec.rb
+++ spec/current_player_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require_relative '../lib/current_player.rb'
 
-describe "./lib/current_player.rb" do
+describe './lib/current_player.rb' do
   describe '#turn_count' do
     it 'counts occupied positions' do
-      board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
-      board2 = ["O", " ", " ", " ", "X", " ", " ", "O", "X"]
+      board1 = ['O', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X']
+      board2 = ['O', ' ', ' ', ' ', 'X', ' ', ' ', 'O', 'X']
 
       expect(turn_count(board1)).to eq(3)
       expect(turn_count(board2)).to eq(4)
@@ -13,21 +15,21 @@ describe "./lib/current_player.rb" do
 
   describe '#current_player' do
     it 'returns the correct player, X, for the first move' do
-      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+      board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-      expect(current_player(board)).to eq("X")
+      expect(current_player(board)).to eq('X')
     end
 
     it 'returns the correct player, O, for the second move' do
-      board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+      board = [' ', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ']
 
-      expect(current_player(board)).to eq("O")
+      expect(current_player(board)).to eq('O')
     end
 
     it 'returns the correct player, X, for the third move' do
-      board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+      board = ['O', ' ', ' ', ' ', 'X', ' ', ' ', ' ', ' ']
 
-      expect(current_player(board)).to eq("X")
+      expect(current_player(board)).to eq('X')
     end
-  end  
+  end
 end
