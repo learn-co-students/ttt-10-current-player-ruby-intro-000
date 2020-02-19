e90dfe32b0570fe8b832c7110485ae4d1199ace7
Leonora Squires <leonora.squires@gmail.com>
2020-02-19 16:01:47 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+def turn_count(board)
+  counter = 0
+  board.each do |i|
+    if i == "X" || i == "O"
+      counter+= 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
