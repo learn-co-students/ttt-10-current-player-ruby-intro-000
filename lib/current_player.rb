1dffd778b1d33a229360dc21cbf0c398260433df
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-03-17 02:16:34 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,31 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def turn_count(board)
+  counter = 0
+    board.each do |turn|
+        if turn == "X" || turn == "O"
+          counter += 1
+        end
+    end
+  return counter
+end
+
+# def current_player(board)
+#   if turn_count(board) % 2 != 0
+#     return "O"
+#   else
+#     return "X"
+#   end
+# end
+
+# def current_player(board)
+#   if turn_count(board).odd?
+#     return "O"
+#   else
+#     return "X"
+#   end
+# end
+
+def current_player(board)
+  turn_count(board).odd? ? "O" : "X"
+end
