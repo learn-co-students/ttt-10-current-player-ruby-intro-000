50d93d3ba229be4c12a49a845cc2dad8a17064fd
Jonathan East <jonathan_east@ymail.com>
2020-04-02 18:28:22 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,11 +9,5 @@ end
 
 
 def current_player(board)
-  if turn_count(board).even?
-    return "X"
-  elsif turn_count(board).odd?
-    return "O"
-  else
-    return false
-  end
+  turn_count(board).even? ? "X" : "O"
 end
