0b052c889c796bdd2686e8fec4de3d151482530c
Akil Griffiths <akilgg@gmail.com>
2020-04-15 18:11:06 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+def current_player(board)
+turns=turn_count(board)
+  if turns.odd? == true
+     "O"
+  else
+     "X"
+  end
+end
+
+def turn_count(board)
+turns = 0
+board.each do |space|
+
+  if space == "X" || space == "O"
+    turns +=1
+
+  end
+
+end
+return turns
+end
