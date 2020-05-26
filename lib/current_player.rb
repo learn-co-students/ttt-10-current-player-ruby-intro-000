c5d25aec25104f297012733ca01b8a9485e0367b
Jasmine Lok <jasminellok@gmail.com>
2020-05-26 21:04:38 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+def turn_count(board)
+  counter = 0
+  board.each {|space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  }
+  counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0? "X" : "O"
+end
