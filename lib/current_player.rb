f981de085c37558af29e683c8bae352825e7b0ab
Jina Strickland <jina30@gmail.com>
2020-06-21 01:50:53 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,31 @@
+board = ["O", " ", " ", " ", " ", " ", " ", "X", " "]
+
+def turn_count(board)
+  board.count{|board| board == "X" || board == "O"}
+end
+
+
+def current_player(board)
+  turn_count(board) % 2 == 0? "X":"O"
+end
+
+
+
+
+=begin
+
+if turn_count(board) is even number then returns "X"
+if turn_count(board) is odd number then returns "O"
+
+if turn_count is even number
+  puts "X"
+else
+  puts "O"
+
+
+board = ["O", " ", " ", " ", " ", " ", " ", "X", " "]
+board.each{|board| puts "There's #{board} on here"}
+
+value = "X" || "O"
+
+=end
