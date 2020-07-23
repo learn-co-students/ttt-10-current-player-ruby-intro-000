38e16353c8fd7e46edc9b3db08e3824cddce4502
Eli Cusic <tntitans31@gmail.com>
2020-07-23 20:40:52 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,14 @@
 def turn_count(board)
   counter = 0
-  board.each do |x|
-    if x == "X" || x == "O"
-      counter += 1
-    end
-  end
+  board.each{|item| item == "X" || item == "O" ? counter += 1 : counter}
   return counter
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    return "X"
-  else
-    return "O"
-  end  
+  turn_count(board) % 2 == 0 ? "X" : "O"
+  # if turn_count(board) % 2 == 0
+  #   return "X"
+  # else
+  #   return "O"
+  # end
 end
