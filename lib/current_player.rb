589706485f50a81434f88129b11d5b041f35f5ac
Will Vetter <vetter.william@gmail.com>
2020-08-02 00:29:11 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  counter = 0
+
+  board.each do |space|
+    if ![" ", '', nil].include? space
+      counter += 1
+    end
+  end
+  return counter
+end
+
+
+def current_player(board)
+  if turn_count(board).even?
+    return 'X'
+  else
+    return 'O'
+  end
+end
