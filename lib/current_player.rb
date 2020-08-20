1ac005a641c5f96adee30545b7975f0a85201021
Hector <hectorfly21@gmail.com>
2020-08-20 10:48:41 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+counter = 0
+board.each do |player|
+ if (player == "X") || (player == "O")
+   counter += 1
+  puts "#{counter}"
+   end
+ end
+ counter
+end
+
+def current_player(board)
+turn_count(board) % 2 == 0 ? "X" : "O"
+end
