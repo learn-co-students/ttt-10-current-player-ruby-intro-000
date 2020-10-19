5c9be882e0203425290b2d73fca015928c0188ee
Ian Fleming <iantfleming1992@gmail.com>
2020-10-19 09:50:48 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  counter = 0
+  board.each do |space| 
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  end
+  counter  
+end
+
+
+def current_player(board)
+  num = turn_count(board)
+  if num % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
