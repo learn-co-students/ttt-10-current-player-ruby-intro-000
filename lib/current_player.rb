965c85a5c0d4b3fd0cd1b3331ad10b882c58a6f6
Andre Powers <andre@stuypend.com>
2021-01-26 14:55:27 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,10 @@
+def turn_count(board)
+  turns = 0
+  board.each do |spaces|
+    spaces == "X" || spaces == "O" ? turns +=1 : turns +=0
+  return turns
+end
+
+def current_player(board)
+  turn_count(board).odd? ? return "X" : return "O"
+end
