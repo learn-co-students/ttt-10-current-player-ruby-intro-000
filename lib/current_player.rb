d83e14654c327ef847af67ad7c9b626bd0c09dbb
Anna <ajlukasik@gmail.com>
2021-02-05 19:46:53 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,9 +12,10 @@ turns
 end
 
 def current_player (board)
-  if turn_count(board) % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
+  # if turn_count(board) % 2 == 0
+  #   return "X"
+  # else
+  #   return "O"
+  # end
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
