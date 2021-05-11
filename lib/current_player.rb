be3e1ffeae4383d81089da2a50e386f02ed4ac20
Oluwaseyi Olufemi <oluwaseyi.olufemi.e@gmail.com>
2021-05-11 03:05:00 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
